1e61a1abdb396d64a1eeeace963dcc82adeca2da
Simone Zanin <zabi94@gmail.com>
2017-07-17 20:47:58 +0200

    Recipes and Lang
    Updated locales
    Added base recipes

--- src/main/java/zabi/minecraft/covens/common/crafting/VanillaRecipes.java
+++ src/main/java/zabi/minecraft/covens/common/crafting/VanillaRecipes.java
@@ -25,8 +25,46 @@ public class VanillaRecipes {
 				Ingredient.fromStacks(new ItemStack(Items.CLAY_BALL)),
 				Ingredient.fromStacks(new ItemStack(Items.DYE,1,EnumDyeColor.YELLOW.getDyeDamage()))
 				);
-		
-		GameRegistry.addShapedRecipe(rl("chimney"), null, new ItemStack(ModBlocks.chimney), " c ", "ccc", "c c", 'c', Blocks.COBBLESTONE);
+		GameRegistry.addShapedRecipe(rl("chimney"), null, new ItemStack(ModBlocks.chimney), " c ", "ccc", "cbc", 'c', Blocks.COBBLESTONE, 'b', Items.BUCKET);
+		GameRegistry.addShapedRecipe(rl("planks_elder"), null, new ItemStack(ModBlocks.elderPlanks,4), "w", 'w', ModBlocks.log_elder);
+		GameRegistry.addShapedRecipe(rl("planks_yew"), null, new ItemStack(ModBlocks.yewPlanks,4), "w", 'w', ModBlocks.log_yew);
+		GameRegistry.addShapedRecipe(rl("planks_juniper"), null, new ItemStack(ModBlocks.juniperPlanks,4), "w", 'w', ModBlocks.log_juniper);
+		GameRegistry.addShapedRecipe(rl("altar"), null, new ItemStack(ModBlocks.altar), 
+				"ccc", "sos", "sos", 
+				'c', new ItemStack(Blocks.CARPET,1,14),
+				's', Blocks.STONE,
+				'o', new ItemStack(ModItems.misc,1,2)
+				);
+		GameRegistry.addShapedRecipe(rl("talisman"), null, new ItemStack(ModItems.misc,1,8), 
+				"ini", "ndn", "ini",
+				'i', Items.IRON_INGOT,
+				'n', Items.IRON_NUGGET,
+				'd', Items.DIAMOND
+				);
+		GameRegistry.addShapedRecipe(rl("talisman_rot"), null, new ItemStack(ModItems.misc,1,8), 
+				"nin", "idi", "nin",
+				'i', Items.IRON_INGOT,
+				'n', Items.IRON_NUGGET,
+				'd', Items.DIAMOND
+				);
+		GameRegistry.addShapedRecipe(rl("chalk"), null, new ItemStack(ModItems.chalk), 
+				"bw ", "wfw", " wb",
+				'b', new ItemStack(Items.DYE,1,EnumDyeColor.WHITE.getDyeDamage()),
+				'w', new ItemStack(ModItems.misc,1,7),
+				'f', new ItemStack(ModItems.flowers,1,2)
+				);
+		GameRegistry.addShapelessRecipe(rl("cauldron"), null, new ItemStack(ModBlocks.cauldron), 
+				Ingredient.fromItem(Items.CAULDRON),
+				Ingredient.fromStacks(new ItemStack(ModItems.misc,1,5)),
+				Ingredient.fromStacks(new ItemStack(ModItems.flowers,1,1)),
+				Ingredient.fromStacks(new ItemStack(ModItems.misc,1,1))
+				);
+		GameRegistry.addShapedRecipe(rl("cardinal_stone"), null, new ItemStack(ModItems.cardinal_stone), 
+				"asa", "ses", "sss",
+				'a', new ItemStack(ModItems.misc,1,1),
+				's', Blocks.MOSSY_COBBLESTONE,
+				'e', new ItemStack(ModItems.misc,1,4)
+				);
 	}
 
 	private static void registerFurnaceRecipes() {
--- src/main/java/zabi/minecraft/covens/common/item/ItemCardinalStone.java
+++ src/main/java/zabi/minecraft/covens/common/item/ItemCardinalStone.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import zabi.minecraft.covens.common.lib.Reference;
@@ -24,6 +26,7 @@ public class ItemCardinalStone extends Item {
 		this.setRegistryName(Reference.MID, "cardinal_stone");
 		this.setMaxStackSize(1);
 		this.setCreativeTab(ModCreativeTabs.products);
+		this.setHasSubtypes(true);
 	}
 	
 	@Override
@@ -32,6 +35,18 @@ public class ItemCardinalStone extends Item {
 		NBTTagCompound pos = stack.getOrCreateSubCompound("pos");
 		if (pos.hasKey("x")) {
 			tooltip.add(TextFormatting.GRAY + TextFormatting.ITALIC.toString() + I18n.format("item.cardinal_stone.bound_to", (int) pos.getDouble("x"), (int) pos.getDouble("y"), (int) pos.getDouble("z"), pos.getInteger("dim")));
+		} else if (stack.getMetadata()==1) {
+			tooltip.add(TextFormatting.GRAY + TextFormatting.ITALIC.toString() + I18n.format("item.cardinal_stone.unbound"));
+		} else if (stack.getMetadata()==0) {
+			tooltip.add(TextFormatting.GRAY + TextFormatting.ITALIC.toString() + I18n.format("item.cardinal_stone.uncharged"));
+		}
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items) {
+		if (this.isInCreativeTab(tab)) {
+			items.add(new ItemStack(this,1,0));
+			items.add(new ItemStack(this,1,1));
 		}
 	}
 	
