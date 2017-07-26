d15dcf304d7e67c73cbc0258e00fac7b582b0343
Simone Zanin <zabi94@gmail.com>
2017-07-26 16:55:11 +0200

    Added remaining brooms
    Juniper is very precise and can brake, yew teleports you and itself back
    to where you mounted it

--- src/main/java/zabi/minecraft/covens/common/entity/EntityFlyingBroom.java
+++ src/main/java/zabi/minecraft/covens/common/entity/EntityFlyingBroom.java
@@ -18,6 +18,7 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityMountEvent;
@@ -25,11 +26,16 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import zabi.minecraft.covens.common.item.ModItems;
+import zabi.minecraft.covens.common.lib.Log;
 
 @Mod.EventBusSubscriber
 public class EntityFlyingBroom extends Entity {
 	
 	private static final DataParameter<Integer> TYPE = EntityDataManager.<Integer>createKey(EntityFlyingBroom.class, DataSerializers.VARINT);
+	private static final DataParameter<Integer> ORIG_X = EntityDataManager.<Integer>createKey(EntityFlyingBroom.class, DataSerializers.VARINT);
+	private static final DataParameter<Integer> ORIG_Y = EntityDataManager.<Integer>createKey(EntityFlyingBroom.class, DataSerializers.VARINT);
+	private static final DataParameter<Integer> ORIG_Z = EntityDataManager.<Integer>createKey(EntityFlyingBroom.class, DataSerializers.VARINT);
+	
 	
 	public EntityFlyingBroom(World world) {
 		super(world);
@@ -49,10 +55,26 @@ public class EntityFlyingBroom extends Entity {
 	public double getMountedYOffset() {
 		return 0.4;
 	}
+	
+	private BlockPos getMountPos() {
+		return new BlockPos(this.getDataManager().get(ORIG_X), this.getDataManager().get(ORIG_Y), this.getDataManager().get(ORIG_Z));
+	}
+	
+	public void setMountPos(BlockPos pos) {
+		this.getDataManager().set(ORIG_X, pos.getX());
+		this.getDataManager().set(ORIG_Y, pos.getY());
+		this.getDataManager().set(ORIG_Z, pos.getZ());
+		this.getDataManager().setDirty(ORIG_X);
+		this.getDataManager().setDirty(ORIG_Y);
+		this.getDataManager().setDirty(ORIG_Z);
+	}
 
 	@Override
 	protected void entityInit() {
 		this.getDataManager().register(TYPE, 0);
+		this.getDataManager().register(ORIG_X, 0);
+		this.getDataManager().register(ORIG_Y, 0);
+		this.getDataManager().register(ORIG_Z, 0);
 	}
 	
 	@Override
@@ -96,17 +118,22 @@ public class EntityFlyingBroom extends Entity {
 		this.motionX*=friction;
 		this.motionY*=friction;
 		this.motionZ*=friction;
-		
+		EntityPlayer rider = (EntityPlayer) this.getControllingPassenger();
 		if (this.isBeingRidden()) {
-			EntityPlayer rider = (EntityPlayer) this.getControllingPassenger();
-			if (rider==null) return;
+			if (rider==null) {
+				Log.w(this+" is being ridden by a null rider!");
+				return;
+			}
 			float front = rider.moveForward, up = rider.isJumping?1:0, strafe = rider.moveStrafing;
 			Vec3d look = rider.getLookVec();
 			
 			if (broomType==0) handleMundaneMovement(front, look);
 			if (broomType==1) handleElderMovement(front, up, strafe, look);
 			if (broomType==2) handleJuniperMovement(front, up, strafe, look);
-			if (broomType==3) handleYewMovement(front, up, strafe, look);
+			if (broomType==3) {
+				rider.noClip = true;
+				handleYewMovement(front, up, strafe, look);
+			}
 			this.setRotationYawHead(rider.rotationYaw);
 			
 		} else {
@@ -128,10 +155,27 @@ public class EntityFlyingBroom extends Entity {
 	}
 	
 	private void handleYewMovement(float front, float up, float strafe, Vec3d look) {
-		
+		handleMundaneMovement(front, look);
 	}
 
 	private void handleJuniperMovement(float front, float up, float strafe, Vec3d look) {
+		if (front>=0) {
+			Vec3d horAxis = look.crossProduct(new Vec3d(0, 1, 0)).normalize().scale(-strafe/10);
+			motionX += front*(horAxis.x+look.x)/80;
+			motionZ += front*(horAxis.z+look.z)/80;
+			motionY += (up/80 + front*(horAxis.y+look.y)/80);
+
+			if (motionX*motionX + motionY*motionY + motionZ*motionZ > 1) {
+				Vec3d limit = new Vec3d(motionX,motionY,motionZ).normalize().scale(2);
+				motionX=limit.x;
+				motionY=limit.y;
+				motionZ=limit.z;
+			}
+		} else {
+			motionX/=1.1;
+			motionY/=1.1;
+			motionZ/=1.1;
+		}
 		
 	}
 	
@@ -227,6 +271,7 @@ public class EntityFlyingBroom extends Entity {
 			player.rotationYaw = this.rotationYaw;
 	        player.rotationPitch = this.rotationPitch;
 			player.startRiding(this);
+			if (getType()==3) this.setMountPos(player.getPosition());
 			return EnumActionResult.SUCCESS;
 		}
 		return EnumActionResult.PASS;
@@ -243,14 +288,20 @@ public class EntityFlyingBroom extends Entity {
 	@SubscribeEvent(receiveCanceled=false, priority=EventPriority.LOWEST)
 	public static void onUnmounting(EntityMountEvent evt) {
 		if (evt.getEntityBeingMounted() instanceof EntityFlyingBroom) {
-			World world = evt.getEntityBeingMounted().world;
+			EntityFlyingBroom broom = (EntityFlyingBroom)evt.getEntityBeingMounted();
+			World world = broom.world;
 			EntityPlayer source = (EntityPlayer) evt.getEntityMounting();
 			if (evt.isDismounting()) {
-				if (((EntityFlyingBroom)evt.getEntityBeingMounted()).getType()==1) evt.getEntityMounting().fallDistance = -200;
-				if (!world.isRemote) {
-					EntityItem ei = new EntityItem(world, source.posX, source.posY, source.posZ, new ItemStack(ModItems.broom,1, ((EntityFlyingBroom)evt.getEntityBeingMounted()).getType()));
+				if (broom.getType()==1) evt.getEntityMounting().fallDistance = -200;
+				if (broom.getType()==3) {
+					BlockPos start = broom.getMountPos();
+					broom.setPositionAndUpdate(start.getX(), start.getY(), start.getZ());
+					source.attemptTeleport(start.getX(), start.getY(), start.getZ());
+				}
+				if (!world.isRemote) { //TODO check for owl
+					EntityItem ei = new EntityItem(world, source.posX, source.posY, source.posZ, new ItemStack(ModItems.broom,1, broom.getType()));
 					world.spawnEntity(ei);
-					evt.getEntityBeingMounted().setDead();
+					broom.setDead();
 					ei.onCollideWithPlayer(source);
 				}
 			}
