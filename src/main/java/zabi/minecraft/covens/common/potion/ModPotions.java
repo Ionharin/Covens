d18bb58361d6c6740cf9de6814667e881f4eca4a
Simone Zanin <zabi94@gmail.com>
2017-08-22 17:38:27 +0200

    Potion of Extinguishing fire

--- src/main/java/zabi/minecraft/covens/common/potion/ModPotions.java
+++ src/main/java/zabi/minecraft/covens/common/potion/ModPotions.java
@@ -7,25 +7,27 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
 import zabi.minecraft.covens.common.lib.Log;
 import zabi.minecraft.covens.common.potion.potions.PotionDisrobing;
+import zabi.minecraft.covens.common.potion.potions.PotionExtinguishFire;
 import zabi.minecraft.covens.common.potion.potions.PotionSkinRotting;
 import zabi.minecraft.covens.common.potion.potions.PotionTinting;
 
 @Mod.EventBusSubscriber
 public class ModPotions {
 	
-	public static ModPotion disrobing, tinting, skin_rotting;
+	public static ModPotion disrobing, tinting, skin_rotting, extinguish_fire;
 	
 	public static void registerAll() {
 		Log.i("Creating potions");
 		disrobing = new PotionDisrobing(0xb40eff, "disrobing");
 		tinting = new PotionTinting(0xffff99, "tinting");
 		skin_rotting = new PotionSkinRotting(0x766d1b, "skin_rotting");
+		extinguish_fire = new PotionExtinguishFire(0x008080, "extinguish_fire");
 	}
 	
 	@SubscribeEvent
 	public static void registerPotions(RegistryEvent.Register<Potion> evt) {
 		Log.i("Registering potions");
 		IForgeRegistry<Potion> reg = evt.getRegistry();
-		reg.registerAll(disrobing, tinting, skin_rotting);
+		reg.registerAll(disrobing, tinting, skin_rotting, extinguish_fire);
 	}
 }
--- src/main/java/zabi/minecraft/covens/common/potion/potions/PotionExtinguishFire.java
+++ src/main/java/zabi/minecraft/covens/common/potion/potions/PotionExtinguishFire.java
@@ -1,11 +1,25 @@
 package zabi.minecraft.covens.common.potion.potions;
 
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.DamageSource;
 import zabi.minecraft.covens.common.potion.ModPotion;
 
 public class PotionExtinguishFire extends ModPotion {
 
-	protected PotionExtinguishFire(int liquidColorIn, String name) {
+	public PotionExtinguishFire(int liquidColorIn, String name) {
 		super(false, liquidColorIn, name);
+		this.setIconIndex(2, 0);
+	}
+	
+	@Override
+	public boolean isReady(int duration, int amplifier) {
+		return duration%10==0;
+	}
+	
+	@Override
+	public void performEffect(EntityLivingBase entity, int amplifier) {
+		if (entity.isBurning()) entity.extinguish();
+		if (entity.isImmuneToFire()) entity.attackEntityFrom(DamageSource.MAGIC, 1f);
 	}
 
 }
