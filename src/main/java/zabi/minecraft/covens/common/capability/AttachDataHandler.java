449c48faf08ab0c300a1eb9750b0c534388c90c2
Simone Zanin <zabi94@gmail.com>
2017-09-13 21:12:58 +0200

    Added natural regen to infusion power if below half
    Very slow, not constant but randomized

--- src/main/java/zabi/minecraft/covens/common/capability/AttachDataHandler.java
+++ src/main/java/zabi/minecraft/covens/common/capability/AttachDataHandler.java
@@ -5,14 +5,17 @@ import zabi.minecraft.covens.common.lib.Reference;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import zabi.minecraft.covens.common.Covens;
+import zabi.minecraft.covens.common.network.messages.InfusionPowerChanged;
 import zabi.minecraft.covens.common.network.messages.SyncEntityDataRequest;
 import zabi.minecraft.covens.common.network.messages.SyncPlayerDataRequest;
 
@@ -24,10 +27,8 @@ public class AttachDataHandler {
 	@SubscribeEvent
 	public void attachCapability(AttachCapabilitiesEvent<Entity> event) {
 		if (event.getObject() instanceof EntityLivingBase) {
-//			Log.d("Injecting entityLivingBase capabilities");
 			event.addCapability(ENTITY_DATA, new EntityData.Provider());
 			if (event.getObject() instanceof EntityPlayer) {
-//				Log.d("Injecting entityPlayer capabilities");
 				event.addCapability(PLAYER_DATA, new PlayerData.Provider());
 			}
 		}
@@ -60,4 +61,18 @@ public class AttachDataHandler {
 			}
 		}
 	}
+	
+	@SubscribeEvent
+	public void onLivingTick(PlayerTickEvent evt) {
+		if (evt.side==Side.SERVER) {
+			if (evt.player.ticksExisted%20==0 && evt.player.getRNG().nextDouble()<0.01) {
+				PlayerData data = evt.player.getCapability(PlayerData.CAPABILITY, null);
+				if (data.getInfusion()!=null && data.getInfusionPower()<data.getMaxPower()/2) {
+					data.setPower(data.getInfusionPower()+1);
+					Covens.network.sendTo(new InfusionPowerChanged(evt.player, data.getInfusionPower()), (EntityPlayerMP) evt.player);
+				}
+			}
+		}
+	}
+	
 }
