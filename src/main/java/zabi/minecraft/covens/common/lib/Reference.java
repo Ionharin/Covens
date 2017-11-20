fb363f6ec2cdca8b24fb183293006d9f119e7858
Simone Zanin <zabi94@gmail.com>
2017-11-20 19:06:29 +0100

    Covens-0.1.30-dev-patron

--- src/main/java/zabi/minecraft/covens/common/lib/Reference.java
+++ src/main/java/zabi/minecraft/covens/common/lib/Reference.java
@@ -4,9 +4,9 @@ public class Reference {
 
 	public static final String MID = "covens";
 	public static final String NAME = "Covens";
-	public static final String VERSION = "0.1.29-dev";
+	public static final String VERSION = "0.1.30-dev-patron";
 	
-	public static final int release = 29;
+	public static final int release = 30;
 	
 	public static final String PROXY_CLIENT = "zabi.minecraft.covens.client.proxy.ClientProxy";
 	public static final String PROXY_SERVER = "zabi.minecraft.covens.common.proxy.ServerProxy";
