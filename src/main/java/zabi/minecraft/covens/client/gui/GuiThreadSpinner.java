2eacc0ed5aacd6385663033372e14f699d5618b8
Simone Zanin <zabi94@gmail.com>
2017-11-28 21:56:01 +0100

    Fixed Thread Spinner GUI

--- src/main/java/zabi/minecraft/covens/client/gui/GuiThreadSpinner.java
+++ src/main/java/zabi/minecraft/covens/client/gui/GuiThreadSpinner.java
@@ -1,21 +1,20 @@
 package zabi.minecraft.covens.client.gui;
 
+import zabi.minecraft.covens.common.lib.Reference;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.inventory.Container;
 import net.minecraft.util.ResourceLocation;
-import zabi.minecraft.covens.common.lib.Reference;
+import zabi.minecraft.covens.common.inventory.ContainerThreadSpinner;
 import zabi.minecraft.covens.common.tileentity.TileEntityThreadSpinner;
 
 public class GuiThreadSpinner extends GuiContainer {
 	
 	private static final ResourceLocation texture = new ResourceLocation(Reference.MID, "textures/gui/thread_spinner.png");
 	
-	private TileEntityThreadSpinner te;
-
 	public GuiThreadSpinner(Container inventorySlotsIn, TileEntityThreadSpinner te) {
 		super(inventorySlotsIn);
-		this.te=te;
 		this.xSize=176;
 		this.ySize=166;
 	}
@@ -25,7 +24,8 @@ public class GuiThreadSpinner extends GuiContainer {
 		this.drawDefaultBackground();
 		Minecraft.getMinecraft().renderEngine.bindTexture(texture);
 		drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
-		double progress = (double) te.getTickProgress() / (double) (TileEntityThreadSpinner.MAX_TICKS - 10);
+		ContainerThreadSpinner c = (ContainerThreadSpinner) this.inventorySlots;
+		double progress = (double) c.data_a[0] / (double) (TileEntityThreadSpinner.MAX_TICKS - 10);
 		drawTexturedModalRect(guiLeft+85, guiTop+33, 176, 0, (int)(22*progress), 17);
 	}
 	
